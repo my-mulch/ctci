823add4ea8117860b7430959ebf062f3934fee4d
Truman Purnell <truman.purnell@gmail.com>
2018-10-17 00:16:55 -0500

    implements heap sort

--- tools/heap.py
+++ tools/heap.py
@@ -1,36 +1,51 @@
 from misc import swap
+import numpy as np
 
 class Heap():
 
     def __init__(self, data):
-        self.heap = Heap.build_max_heap(data)
+        self.data = data
+        self.size = len(self.data)
     
     @staticmethod
-    def build_max_heap(data):
+    def build_max_heap(heap):
         # from i / 2 downto 0 as everything beyond are already leaves
         # and thus satisfy the max heap property
-        for i in reversed(range(len(data) // 2)): 
-            Heap.max_heapify(data, i)
-        
-        return data
+        for i in reversed(range(heap.size // 2)): 
+            Heap.max_heapify(heap, i)
 
     @staticmethod
-    def max_heapify(arr, i):
-        if i < len(arr) // 2:
+    def max_heapify(heap, i):
+        if i < heap.size // 2:
             lci = 2 * i + 1
             rci = 2 * i + 2
             
-            lc = arr[lci]
-            rc = arr[rci] if rci < len(arr) else lc
-            ci = arr[i]
+            lc = heap.data[lci]
+            rc = heap.data[rci] if rci < heap.size else lc
+            ci = heap.data[i]
             
             max_i = lci if lc >= rc else rci
-            max_c = arr[max_i]
+            max_c = heap.data[max_i]
 
             if ci >= max_c:
                 return
         
-            swap(arr, i, max_i)
-            Heap.max_heapify(arr, max_i)
+            swap(heap.data, i, max_i)
+            Heap.max_heapify(heap, max_i)
             
+    def extract_max(self):
+        max_val = self.data[0]
+        
+        self.data[0] = self.data[self.size - 1]
+        self.size -= 1
 
+        Heap.max_heapify(self, 0)
+        return max_val
+    
+    def sort(self):
+        while self.size:
+            yield self.extract_max()
+
+heap = Heap(np.random.randn(100000).tolist())
+Heap.build_max_heap(heap)
+list(heap.sort())
