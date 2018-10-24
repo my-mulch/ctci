e3b3b7e127588e3b4ebe470efa168a50e870b1af
Truman Purnell <truman.purnell@gmail.com>
2018-10-24 18:37:47 -0500

    adds delete method using general traversal callback fn

--- tools/structures/link.py
+++ tools/structures/link.py
@@ -12,6 +12,7 @@ class LinkedList():
             callback.state += "- |  {}  | -".format(node.data)
         
         callback.state = ''
+        callback.done = False
 
         return self.traverse(callback)
 
@@ -20,11 +21,38 @@ class LinkedList():
         
         while(runner):
             fn(runner)
+            
+            if fn.done:
+                break
+
             runner = runner.next
         
         return fn.state
 
+    def delete(self, data):
+        def callback(node):
+            if node.data == data:
+                callback.done = True
+
+                if node.prev and node.next:
+                    node.prev.next = node.next
+                    node.next.prev = node.prev
+
+                elif node.prev is None: # trying to delete head
+                    self.head = self.head.next
+                    if self.head:
+                        self.head.prev = None
+
+                else: # trying to delete tail
+                    self.tail = self.tail.prev
+                    if self.tail:
+                        self.tail.next = None
         
+        callback.state = ''
+        callback.done = False
+        
+        self.traverse(callback)
+
 
     def insert(self, node, insertion_point):
         if type(node) is not LinkNode:
@@ -52,6 +80,14 @@ dll.insert(10, 'tail')
 dll.insert(17, 'tail')
 dll.insert(13, 'head')
 
+dll.delete(13)
+dll.delete(17)
+dll.delete(10)
+
+dll.insert(11, 'head')
+dll.insert(47, 'head')
+dll.insert(22, 'head')
+dll.insert(22, 'tail')
 
 print(dll)
 
