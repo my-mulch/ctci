277793c8bdcbc54a1eb536da16615c97a031500c
Truman Purnell <truman.purnell@gmail.com>
2018-10-24 22:23:35 -0500

    solves chapter 2 problem 4

--- desk.py
+++ desk.py
@@ -1,19 +1,21 @@
 from problems import chapter2
 from tools.structures.link import LinkedList
 
-dll = LinkedList(node_variety='single')
+dll = LinkedList(node_variety='double')
 
-dll.insert(10, 'tail')
-dll.insert(17, 'tail')
-
-mid = dll.insert(11, 'tail')
-
-dll.insert(17, 'tail')
-dll.insert(22, 'tail')
-dll.insert(22, 'tail')
+dll.insert(9, 'tail')
+dll.insert(3, 'tail')
+dll.insert(8, 'tail')
+dll.insert(3, 'tail')
+dll.insert(1, 'tail')
+dll.insert(3, 'tail')
+dll.insert(6, 'tail')
+dll.insert(3, 'tail')
+dll.insert(4, 'tail')
+dll.insert(7, 'tail')
 
 print(dll)
 
-chapter2.delete_mid_node(mid)
+chapter2.partition(dll, 4)
 
 print(dll)
--- problems/chapter2.py
+++ problems/chapter2.py
@@ -35,3 +35,19 @@ def delete_mid_node(mid_node):
     next_node = mid_node.next
     mid_node.data = next_node.data
     mid_node.next = next_node.next
+
+def partition(linked_list, p):
+    runner = linked_list.head
+    
+    while(runner):
+        if runner.data < p:
+            linked_list.insert(runner.data, 'head')
+
+            if runner.prev:	
+                runner.prev.next = runner.next
+            if runner.next:
+                runner.next.prev = runner.prev
+        
+        runner = runner.next
+
+
