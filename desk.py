49f32c3ceeb7ae26ff37acfc99ced02d2d39718f
Truman Purnell <truman.purnell@gmail.com>
2018-10-25 21:22:15 -0500

    complete chapter 3 problem 3

--- desk.py
+++ desk.py
@@ -1,1 +1,10 @@
 from problems import chapter3
+import numpy as np
+
+stack = chapter3.MegaStack()
+
+nums = np.random.randint(0,10,1000)
+
+for num in nums:
+    getattr(stack, 'pop' if np.random.rand() < 0.5 else 'push')(num)
+    print(stack.stacks)
--- problems/chapter3.py
+++ problems/chapter3.py
@@ -1,26 +1,50 @@
 import math
 
-class MinStack():
 
+class MinStack():
     def __init__(self):
         self.min = math.inf
-        
+
         self.prev_mins = []
         self.stack = []
-    
+
     def push(self, data):
         if data < self.min:
             self.min = data
             self.prev_mins.append(self.min)
-        
+
         self.stack.append(data)
-    
+
     def pop(self):
         popped = self.stack.pop()
-        
-        if popped  == self.prev_mins[-1]:
+
+        if popped == self.prev_mins[-1]:
             self.prev_mins.pop()
             self.min = self.prev_mins[-1]
-        
+
         return popped
 
+
+class MegaStack():
+    def __init__(self, cap=10):
+        self.stacks = []
+        self.cap = cap
+
+    def push(self, data):
+        if not self.stacks or len(self.stacks[-1]) == self.cap:
+            self.stacks.append([data])
+            return
+
+        self.stacks[-1].append(data)
+
+    def pop(self, data):
+        if not self.stacks:
+            return
+        
+        if len(self.stacks[-1]) == 0:
+            self.stacks.pop()  # removing empty last stack
+            
+            
+        if self.stacks:
+            return self.stacks[-1].pop()        
+        
