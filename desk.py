2fad66591dff38e0ca0990e52296ad1cccbf6976
Truman Purnell <truman.purnell@gmail.com>
2018-10-31 00:37:20 -0500

    solves chapter 8 problem 6

--- desk.py
+++ desk.py
@@ -1,4 +1,15 @@
 from problems import chapter8
 import numpy as np
 
-print(chapter8.recursive_multiply(8, 4))
+A, B, C = [1,
+           2,
+           3,
+           4,
+           5,
+           6,
+           7,
+           8], [], []
+
+chapter8.towers_of_hanoi(disks=len(A), source=A, buffer=C, target=B)
+
+print(A, B, C)
