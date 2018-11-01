a2778a67770d0fe40508b128a6961a23b7adc85c
Truman Purnell <truman.purnell@gmail.com>
2018-10-31 21:55:48 -0500

    Revert "implements BFS as class method for Graoh"
    This reverts commit d816642dd59c115cd3c64c4fee2bdb7e44c30f23.

--- desk.py
+++ desk.py
@@ -1,3 +1,13 @@
 from problems import chapter17
 import numpy as np
 
+n = 15
+
+nums = [i for i in range(n)]
+np.random.shuffle(nums)
+
+missing_index = np.random.randint(n)
+missing_value = nums[missing_index]
+del nums[missing_index]
+
+assert(missing_value == chapter17.find_missing(nums))
