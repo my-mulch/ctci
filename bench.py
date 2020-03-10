f637114a238249e936e5bdfd991d297ea63aa249
trumanpurnell <truman.purnell@gmail.com>
2020-03-10 00:54:11 -0700

    bench displays something meaningful

--- bench.py
+++ bench.py
@@ -3,16 +3,9 @@ from tools.algorithms.sort import insertion_sort, insertion_sort_bin, heap_sort,
 import numpy as np
 from timeit import Timer
 
-arr = np.random.randint(0,20,1000000)
+arr = np.random.randint(0, 20, 10000).tolist()
 
-timer, label = Timer(lambda: insertion_sort(arr.tolist())), 'insertion-reg'
-print(timer.timeit(1),label)
-
-timer, label = Timer(lambda: insertion_sort_bin(arr.tolist())), 'insertion-bin'
-print(timer.timeit(1),label)
-
-timer, label = Timer(lambda: heap_sort(arr.tolist())), 'heapsort'
-print(timer.timeit(1),label)
-
-timer, label = Timer(lambda: merge_sort(arr.tolist())), 'merge_sort'
-print(timer.timeit(1),label)
+print(Timer(lambda: insertion_sort(arr)).timeit(1), 'insertion-reg')
+print(Timer(lambda: insertion_sort_bin(arr)).timeit(1), 'insertion-bin')
+print(Timer(lambda: heap_sort(arr)).timeit(1), 'heapsort')
+print(Timer(lambda: merge_sort(arr)).timeit(1), 'merge_sort')
